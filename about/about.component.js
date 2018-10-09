6a4bb5259d3f7cb2f8be1309d1d0f7b05ee22156
Alex Stolzoff <amstolzo@ucsd.edu>
2018-10-09 13:19:22 -0700

    testing php email server side code

--- about/about.component.js
+++ about/about.component.js
@@ -2,14 +2,36 @@ angular.
 module('about').
 component('about', {
     templateUrl: "about/about.component.html",
-    controller: ['LangChoice','$scope', function homeController(LangChoice,$scope){
+    controller: ['LangChoice', '$scope', '$http', function homeController(LangChoice, $scope, $http) {
         $scope.langPos = LangChoice.langPos;
-        
         setInterval(function () {
             $scope.$apply(function () {
                 $scope.langPos = LangChoice.langPos;
             });
         }, 0);
-    
+        
+        window.console.log($http.post("about/form-submit.php", "hi"));
+        $scope.submitForm = function () {
+           
+            $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+
+            const name = $("#userName").val();
+            const email = $("#userEmail").val();
+            const message = $("#userMessage").val();
+            data = {
+                'name' : name,
+                'email' : email,
+                'message' : message,
+            };
+            
+            $http.post('about/from-submit.php', data)
+            .success(function(data, status, headers, config){
+                window.console.log(status + ' - ' + data);
+            })
+            .error(function(data, status, headers, config){
+                window.console.log('error');
+            });
+        }
+
 }]
 });
